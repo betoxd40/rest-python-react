934c2e86852e55d678ec280b6fc7650406543e2a
Alberto Yanes <albertoyanesjr3@hotmail.com>
2019-01-16 07:09:09 -0300

    first commit

---
+++ VenvDjango/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alberto/Documents/myGit/python-react/rest-python-react/VenvDjango/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
